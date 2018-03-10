5ea2cc4ebdd2a7920f8a98007cc37a4a0e287966
luigitemu <luartemu@gmail.com>
2018-03-09 20:12:29 -0600

    Add files via upload

---
+++ Registrarse/js/controlador.js
@@ -0,0 +1,8 @@
+function  validacion(){
+		if (document.getElementById("txt-nombre").value=="") {
+			document.getElementById("txt-nombre").classList.add("");
+		}else{
+			console.log("no esta vacion");
+		}
+}
+
